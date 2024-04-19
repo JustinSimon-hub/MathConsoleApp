74d6d37b135c93ab90c78f273067e15daa14517f
Justin Simon <justinsimon@Justins-MBP.lan>
2024-04-19 10:06:22 -0700

    Created 4 functions, going to prompt user with math problems next

--- MathOperationsConsoleApp/Program.cs
+++ MathOperationsConsoleApp/Program.cs
@@ -5,17 +5,75 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // Call static methods directly on the class
-        int sum = Operations.Add(5, 3);
-        int difference = MathUtils.Subtract(10, 4);
-        int product = MathUtils.Multiply(6, 2);
-        double quotient = MathUtils.Divide(8, 2);
+        List<double> results = new List<double>();
 
-        Console.WriteLine($"Sum: {sum}");
-        Console.WriteLine($"Difference: {difference}");
-        Console.WriteLine($"Product: {product}");
-        Console.WriteLine($"Quotient: {quotient}");
+        Console.WriteLine("Please enter a digit for the desired operaton.");
+        Console.WriteLine("--------------");
+        Console.WriteLine("1.Addition");
+        Console.WriteLine("2.Subtraction");
+        Console.WriteLine("3.Multiplication");
+        Console.WriteLine("4.Division");
+        Console.WriteLine("--------------");
+        var ans = Console.ReadLine();
+        while (true)
+        {
+            switch (ans)
+            {
+                case "1":
 
-        Console.ReadKey();
+                    Console.WriteLine("Enter in two integers to add.");
+                    var a = Int32.Parse(Console.ReadLine());
+                    var b = Int32.Parse(Console.ReadLine());
+                    var sum = Operations.Add(a, b);
+                    Console.WriteLine($"The answer to that is {sum}");
+                    results.Add(sum);
+                    break;
+
+
+                case "2":
+                    Console.WriteLine("Enter in two integers to subtract.");
+
+                    var c = Int32.Parse(Console.ReadLine());
+                    var d = Int32.Parse(Console.ReadLine());
+                    var difference = Operations.Subtract(c, d);
+                    Console.WriteLine($"The answer to that is {difference}");
+
+                    results.Add(difference);
+                    break;
+
+                case "3":
+                    Console.WriteLine("Enter in two integers to multiply.");
+                    var e = Int32.Parse(Console.ReadLine());
+                    var f = Int32.Parse(Console.ReadLine());
+                    var product = Operations.Multiply(e, f);
+                    results.Add(product);
+                    Console.WriteLine($"The answer to that is {product}");
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("Enter in two integers to divide.");
+                    var g = Int32.Parse(Console.ReadLine());
+                    var h = Int32.Parse(Console.ReadLine());
+                    var quotient = Operations.Divide(g, h);
+                    results.Add(quotient);
+                    Console.WriteLine($"The answer to that is {quotient}");
+
+                    break;
+
+
+                default:
+                    Console.WriteLine("Please retry your selection of two integers");
+                    break;
+            }
+            Console.WriteLine("----------------");
+            foreach (int x in results)
+            {
+                Console.WriteLine($"The answers to each of the operations were {x}");
+                Console.WriteLine();
+            }
+
+        }
+
     }
 }
